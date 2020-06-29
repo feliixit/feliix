680be4c2a20621ce6cbd445b1189d6609c1022cf
dereckyin <dereckyin@gmail.com>
2020-06-29 21:57:56 +0800

    Add Ammend function  Changes to be committed: 	modified:   ammend.php 	new file:   api/ammend.php 	new file:   js/ammend.js

--- ammend.php
+++ ammend.php
@@ -1,3 +1,4 @@
+<?php include 'check.php';?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -52,7 +53,7 @@ $(function(){
     <!-- header -->
 	<header>header</header>
     <!-- header end -->
-    <div class="mainContent">
+    <div id="app" class="mainContent">
         <!-- tags js在 main.js -->
         <div class="tags">
             <a class="tag A">Attendance</a>
@@ -117,86 +118,82 @@ $(function(){
         <div class="block B focus">
             <h6>Leave Applications</h6>
             <div class="box-content">
-                <div class="title">
-                    <b>Employee Name</b>
-                    <div class="function">
-                        <input name="Foodb" type="radio" value="A" id="A" class="green"><label for="A">All</label>
-                       <input name="Foodb2" type="radio" value="A2" id="A2" class="blue"><label for="A2">Waiting for Approval</label>
-<!--
-                        <b class="light green"></b>All
-                        <b class="light blue"></b>Waiting for Approval
--->
-                        <select name="" id="">
-                            <option value="">MAR / 2020</option>
-                            <option value="">FEB / 2020</option>
-                            <option value="">JAN / 2020</option>
-                        </select>
-                    </div>
-                </div>
-                <i class="hit"><b>WD:</b>Working Day, <b>RD:</b>Resting Day, <b>RH:</b>Regular Day, <b>SH:</b>Special Holiday</i>
+             
                 <div class="tablebox">
                     <ul class="head">
                         <li><i class="micons">view_list</i></li>
-                        <li>Date</li>
-                        <li>Shift</li>
-                        <li>On-Duty Time</li>
-                        <li>Off-Duty Time</li>
+                        <li>Application Time</li>
+                        <li>Applicant</li>
+                        <li>Type</li>
+                        <li>Leave Time</li>
                     </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>RH</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
+                    <ul v-for='(record, index) in displayedRecord' :key="index">
+                        <li>
+                            <input type="checkbox" name="record_id" class="alone blue" :value="record.index" :true-value="1" v-model:checked="record.is_checked">
+                        </li>
+                       
+                        <li>{{ record.created_at }}</li>
+                        <li>{{ record.username }}</li>
+                        <li>{{ record.type }}</li>
+                        <li>{{ record.start_date.substring(0, 4) }}/{{ record.start_date.substring(4, 6) }}/{{ record.start_date.substring(6, 8) }} {{ record.start_time }} - {{ record.end_date.substring(0, 4) }}/{{ record.end_date.substring(4, 6) }}/{{ record.end_date.substring(6, 8) }} {{ record.end_time }} }}</li>
                     </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>RD</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>WD</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>RH</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>SH</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>RD</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-                    <ul>
-                        <li><input name="Foodb2" type="radio" value="A2" id="B2" class="alone blue"></li>
-                        <li>2020/01/07</li>
-                        <li>WD</li>
-                        <li>2020/01/07 9:00 AM</li>
-                        <li>2020/01/07 9:00 AM</li>
-                    </ul>
-
                 </div>
+
+                <div class="btnbox">
+                    <a class="btn" @click="detail">Detail</a>
+                    <a class="btn" @click="apply" :disabled="submit">Withdraw</a>
+                </div>
+
+                <div class="tablebox" v-if="view_detail">
+                    <ul class="head">
+                        <li class="head">Leave Type</li>
+                        <li>{{ (record.leave_type == 'A') ? "Vacation Leave" : ((record.leave_type == 'B') ? "Emerency/Sick Leave" : ((record.leave_type == 'C') ? "Unpaid Leave" : 'Absence')) }}</li>
+                    </ul>
+                    <ul>
+                        <li class="head">Application Time</li>
+                        <li>{{ record.created_at.replace(/-/g,"/").substring(0, 16) }}</li>
+                    </ul>
+                    <ul>
+                        <li class="head">Start Time</li>
+                        <li>{{ record.start_date.substring(0, 4) }}/{{ record.start_date.substring(4, 6) }}/{{ record.start_date.substring(6, 8) }} {{ record.start_time }} </li>
+                    </ul>
+                    <ul>
+                        <li class="head">End Time</li>
+                        <li>{{ record.end_date.substring(0, 4) }}/{{ record.end_date.substring(4, 6) }}/{{ record.end_date.substring(6, 8) }} {{ record.end_time }}</li>
+                    </ul>
+                    <ul>
+                        <li class="head">Leave Length</li>
+                        <li>{{ record.le }} Days</li>
+                    </ul>
+                    <ul>
+                        <li class="head">Reason</li>
+                        <li>{{ record.reason }}</li>
+                    </ul>
+                    <ul v-if="record.pic_url != ''">
+                        <li class="head">Certificate of Diagnosis</li>
+                        <li><i class="fas fa-image"  @click="showPic(record.pic_url)"></i></li>
+                    </ul>
+                </div>
+                
             </div>
             
         </div>
     </div>
 </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> 
+<script src="js/axios.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+<script src="//unpkg.com/vue-i18n/dist/vue-i18n.js"></script>
+<script src="//unpkg.com/element-ui"></script>
+<script src="//unpkg.com/element-ui/lib/umd/locale/en.js"></script>
+
+<script>
+  ELEMENT.locale(ELEMENT.lang.en)
+</script>
+
+<!-- import JavaScript -->
+<script src="https://unpkg.com/element-ui/lib/index.js"></script>
+<script src="js/ammend.js"></script>
 </html>
---
+++ js/ammend.js
@@ -0,0 +1,286 @@
+
+var app = new Vue({
+  el: '#app',
+  data:{
+    name: "",
+    month1:'',
+
+    picked:'A',
+    view_detail:false,
+
+    submit: false,
+
+    receive_records: [],
+    record: {},
+
+  },
+
+  created () {
+    
+    this.getUserName();
+
+  },
+
+  computed: {
+    displayedRecord () {
+      return this.receive_records;
+    },
+
+  },
+
+  mounted(){
+   var d1 = new Date();
+    this.month1 = d1;
+
+    $('#start').val(d1.toISOString().slice(0,7).replace(/-/g,"-"));
+
+    this.getLeaveCredit();
+    
+  },
+
+  watch: {
+
+      picked () {
+        this.getLeaveCredit();
+      },
+  },
+
+
+
+  methods:{
+
+    
+    getLeaveCredit: function() {
+      let _this = this;
+
+      axios.get('api/ammend')
+          .then(function(response) {
+              console.log(response.data);
+              _this.receive_records = response.data;
+
+
+          })
+          .catch(function(error) {
+              console.log(error);
+          });
+  },
+
+    getUserName: function() {
+        var token = localStorage.getItem('token');
+        var form_Data = new FormData();
+        let _this = this;
+
+        form_Data.append('jwt', token);
+
+        axios({
+            method: 'post',
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            url: 'api/on_duty_get_myname',
+            data: form_Data
+        })
+        .then(function(response) {
+            //handle success
+            _this.name = response.data.username;
+            _this.is_manager = response.data.is_manager;
+
+        })
+        .catch(function(response) {
+            //handle error
+            Swal.fire({
+              text: JSON.stringify(response),
+              icon: 'error',
+              confirmButtonText: 'OK'
+            })
+        });
+      },
+
+      unCheckCheckbox()
+        {
+            for (i = 0; i < this.receive_records.length; i++) 
+            {
+              this.receive_records[i].is_checked = false;
+            }
+          //$(".alone").prop("checked", false);
+          //this.clicked = false;
+        },
+
+      showPic(pic)
+      {
+        Swal.fire({
+          title: 'Certificate of Diagnosis',
+          text: 'Click to close',
+          imageUrl: 'img/' + pic,
+        })
+      },
+
+      delReceiveRecord: function(id) {
+            let _this = this;
+            //targetId = this.record.id;
+            var form_Data = new FormData();
+
+            form_Data.append('crud', "del");
+            form_Data.append('id', id);
+
+            var params = {
+                'id': id
+            }
+
+            const token = sessionStorage.getItem('token');
+
+            axios({
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        Authorization: `Bearer ${token}`
+                    },
+                    url: 'api/leave_record_delete',
+                    data: form_Data
+                })
+                .then(function(response) {
+                    //handle success
+                    //this.$forceUpdate();
+                    _this.resetForm();
+                })
+                .catch(function(response) {
+                    //handle error
+                    console.log(response)
+                });
+        },
+
+        detail: function() {
+          let _this = this;
+
+        let favorite = [];
+          
+          for (i = 0; i < this.receive_records.length; i++) 
+            {
+              if(this.receive_records[i].is_checked == 1)
+                favorite.push(this.receive_records[i].id);
+            }
+
+            if (favorite.length != 1) {
+              Swal.fire({
+                text: 'Please select row to see the detail!.',
+                icon: 'warning',
+                confirmButtonText: 'OK'
+              })
+                
+                //$(window).scrollTop(0);
+                this.view_detail = false;
+                return;
+            }
+
+            this.record = this.shallowCopy(this.receive_records.find(element => element.id == favorite));
+
+            this.view_detail = true;
+
+        },
+
+    apply: function() {
+
+      let _this = this;
+
+        let favorite = [];
+
+        var approve_record = false;
+          
+          for (i = 0; i < this.receive_records.length; i++) 
+            {
+              if(this.receive_records[i].is_checked == 1)
+              {
+                if(this.receive_records[i].approval === 'R')
+                {
+                  Swal.fire({
+                    text: 'Rejected data cannot be withdrawn by user! Please contact Admin or IT staffs.',
+                    icon: 'warning',
+                    confirmButtonText: 'OK'
+                  });
+
+                  return;
+                }
+
+                if(this.receive_records[i].approval === 'A' && this.receive_records[i].leave_type === 'D')
+                {
+                  Swal.fire({
+                    text: 'Absence data cannot be withdrawn by user! Please contact Admin or IT staffs.',
+                    icon: 'warning',
+                    confirmButtonText: 'OK'
+                  });
+
+                  return;
+                }
+
+                if(this.receive_records[i].approval === 'A')
+                {
+                  Swal.fire({
+                    text: 'Approved data cannot be withdrawn by user! Please contact Admin or IT staffs.',
+                    icon: 'warning',
+                    confirmButtonText: 'OK'
+                  });
+
+                  return;
+                }
+
+                favorite.push(this.receive_records[i].id);
+              }
+            }
+
+            if (favorite.length < 1) {
+              Swal.fire({
+                text: 'Please select rows to withdraw!.',
+                icon: 'warning',
+                confirmButtonText: 'OK'
+              })
+                
+                //$(window).scrollTop(0);
+                return;
+            }
+
+          
+
+          Swal.fire({
+            title: 'Are you sure to withdraw?',
+            text: "Are you sure to withdraw apply?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes'
+          }).then((result) => {
+            if (result.value) {
+              _this.submit = true;
+              _this.delReceiveRecord(favorite.join(", "));
+
+              _this.resetForm();
+              _this.unCheckCheckbox();
+            }
+          })
+
+      },
+
+      resetForm: function() {
+          
+            this.submit = false;
+            this.view_detail = false;
+
+          this.receive_records = [];
+          this.record = {};
+
+          this.getLeaveCredit();
+
+        },
+
+        shallowCopy(obj) {
+          console.log("shallowCopy");
+            var result = {};
+            for (var i in obj) {
+                result[i] = obj[i];
+            }
+            return result;
+        },
+
+
+  }
+});
