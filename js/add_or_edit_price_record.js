d42a2e252b9b8ca973ee5160f235effd3e31828c
loveiiu <loveiiuyo@gmail.com>
2020-08-31 01:13:34 +0800

    add include conf

--- js/add_or_edit_price_record.js
+++ js/add_or_edit_price_record.js
@@ -630,7 +630,6 @@ var app = new Vue({
 
                   document.body.appendChild(link);
                   link.click();
-                  console.log(response)
               })
               .catch(function(response) {
                   console.log(response)
