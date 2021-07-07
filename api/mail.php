d63f2dabf310cc20f5b0072f45de7ad553d16a8c
dereckyin <dereckyin@yahoo.com.tw>
2021-07-07 10:26:27 +0800

    mail fix and new rq 07/07

--- api/mail.php
+++ api/mail.php
@@ -2427,6 +2427,177 @@ function task_notify01_admin($old_status, $task_status, $revisor, $task_name, $c
 
 }
 
+function task_notify01_admin_sl($old_status, $task_status, $revisor, $task_name, $create_id, $assignee, $collaborator, $due_date, $detail, $stage_id)
+{
+
+    $tab = '<p>Status of task "' . $task_name . '" changed from ' . $old_status . ' to ' . $task_status . '. Following are the details:</p>';
+
+    $conf = new Conf();
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+
+    $mail->SMTPDebug  = 0;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port       = 465;
+    $mail->SMTPKeepAlive = true;
+    $mail->Host       = $conf::$mail_host;
+    $mail->Username   = $conf::$mail_username;
+    $mail->Password   = $conf::$mail_password;
+
+    $mail->IsHTML(true);
+
+    $notifior = array();
+
+    $creators = "";
+    $collaborators = "";
+    $assignees = "";
+
+    $notifior = GetNotifiers($assignee);
+    foreach($notifior as &$list)
+    {
+        $mail->AddAddress($list["email"], $list["username"]);
+        $assignees = $assignees . $list["username"] . ", ";
+    }
+
+    $notifior = GetNotifiers($collaborator);
+    foreach($notifior as &$list)
+    {
+        $mail->AddAddress($list["email"], $list["username"]);
+        $collaborators = $collaborators . $list["username"] . ", ";
+    }
+
+    $notifior = GetNotifiers($create_id);
+    foreach($notifior as &$list)
+    {
+        $mail->AddCC($list["email"], $list["username"]);
+        $creators = $creators . $list["username"] . ", ";
+    }
+
+    $creators = rtrim($creators, ", ");
+    $assignees = rtrim($assignees, ", ");
+    $collaborators = rtrim($collaborators, ", ");
+    
+    $mail->SetFrom("feliix.it@gmail.com", "Feliix.System");
+    $mail->AddReplyTo("feliix.it@gmail.com", "Feliix.System");
+
+    $title = "[Task Notification] Status of " . $task_name . " changed from " . $old_status . ' to ' . $task_status;
+    
+    $mail->Subject = $title;
+    $content =  "<p>Dear all,</p>";
+    $content = $content . $tab;
+    $content = $content . "<p>Task:" . $task_name . "</p>";
+    $content = $content . "<p>Task Status:" . $old_status . ' => ' . $task_status . "</p>";
+    $content = $content . "<p>Creator:" . $creators . "</p>";
+    $content = $content . "<p>Reviser:" . $revisor . "</p>";
+    $content = $content . "<p>Assignee:" . $assignees . "</p>";
+    $content = $content . "<p>Collaborator:" . $collaborators . "</p>";
+    $content = $content . "<p>Due Date:" . $due_date . "</p>";
+    $content = $content . "<p>Description:" . $detail . "</p>";
+    $content = $content . "<p> </p>";
+    $content = $content . "<p>Please click this link to view the target webpage: </p>";
+    $content = $content . "<p>https://feliix.myvnc.com/task_manangement_SLS?sid=" . $stage_id . "</p>";
+
+    $mail->MsgHTML($content);
+    if($mail->Send()) {
+        logMail($creators, $content);
+        return true;
+    } else {
+        logMail($creators, $mail->ErrorInfo . $content);
+        return false;
+    }
+}
+
+function task_notify01_admin_sv($old_status, $task_status, $revisor, $task_name, $create_id, $assignee, $collaborator, $due_date, $detail, $stage_id)
+{
+
+    $tab = '<p>Status of task "' . $task_name . '" changed from ' . $old_status . ' to ' . $task_status . '. Following are the details:</p>';
+
+    $conf = new Conf();
+
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
+    $mail->CharSet = 'UTF-8';
+    $mail->Encoding = 'base64';
+
+    $mail->SMTPDebug  = 0;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Port       = 465;
+    $mail->SMTPKeepAlive = true;
+    $mail->Host       = $conf::$mail_host;
+    $mail->Username   = $conf::$mail_username;
+    $mail->Password   = $conf::$mail_password;
+
+    $mail->IsHTML(true);
+
+    $notifior = array();
+
+    $creators = "";
+    $collaborators = "";
+    $assignees = "";
+
+    $notifior = GetNotifiers($assignee);
+    foreach($notifior as &$list)
+    {
+        $mail->AddAddress($list["email"], $list["username"]);
+        $assignees = $assignees . $list["username"] . ", ";
+    }
+
+    $notifior = GetNotifiers($collaborator);
+    foreach($notifior as &$list)
+    {
+        $mail->AddAddress($list["email"], $list["username"]);
+        $collaborators = $collaborators . $list["username"] . ", ";
+    }
+
+    $notifior = GetNotifiers($create_id);
+    foreach($notifior as &$list)
+    {
+        $mail->AddCC($list["email"], $list["username"]);
+        $creators = $creators . $list["username"] . ", ";
+    }
+
+    $creators = rtrim($creators, ", ");
+    $assignees = rtrim($assignees, ", ");
+    $collaborators = rtrim($collaborators, ", ");
+    
+    $mail->SetFrom("feliix.it@gmail.com", "Feliix.System");
+    $mail->AddReplyTo("feliix.it@gmail.com", "Feliix.System");
+
+    $title = "[Task Notification] Status of " . $task_name . " changed from " . $old_status . ' to ' . $task_status;
+    
+    $mail->Subject = $title;
+    $content =  "<p>Dear all,</p>";
+    $content = $content . $tab;
+    $content = $content . "<p>Task:" . $task_name . "</p>";
+    $content = $content . "<p>Task Status:" . $old_status . ' => ' . $task_status . "</p>";
+    $content = $content . "<p>Creator:" . $creators . "</p>";
+    $content = $content . "<p>Reviser:" . $revisor . "</p>";
+    $content = $content . "<p>Assignee:" . $assignees . "</p>";
+    $content = $content . "<p>Collaborator:" . $collaborators . "</p>";
+    $content = $content . "<p>Due Date:" . $due_date . "</p>";
+    $content = $content . "<p>Description:" . $detail . "</p>";
+    $content = $content . "<p> </p>";
+    $content = $content . "<p>Please click this link to view the target webpage: </p>";
+    $content = $content . "<p>https://feliix.myvnc.com/task_manangement_SVC?sid=" . $stage_id . "</p>";
+
+    $mail->MsgHTML($content);
+    if($mail->Send()) {
+        logMail($creators, $content);
+        return true;
+    } else {
+        logMail($creators, $mail->ErrorInfo . $content);
+        return false;
+    }
+
+}
+
 function task_notify01_admin_d($old_status, $task_status, $revisor, $task_name, $create_id, $assignee, $collaborator, $due_date, $detail, $stage_id)
 {
 
--- api/project03_other_task_edit_sl.php
+++ api/project03_other_task_edit_sl.php
@@ -14,6 +14,9 @@ include_once 'libs/php-jwt-master/src/BeforeValidException.php';
 include_once 'libs/php-jwt-master/src/ExpiredException.php';
 include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-master/src/JWT.php';
+
+include_once 'mail.php';
+
 use \Firebase\JWT\JWT;
 if ( !isset( $jwt ) ) {
     http_response_code(401);
@@ -28,6 +31,7 @@ else
         $decoded = JWT::decode($jwt, $key, array('HS256'));
 
         $user_id = $decoded->data->id;
+        $username = $decoded->data->username;
 
     }
         // if decode fails, it means jwt is invalid
@@ -56,6 +60,34 @@ $assignee = (isset($_POST['assignee']) ?  $_POST['assignee'] : '');
 $collaborator = (isset($_POST['collaborator']) ?  $_POST['collaborator'] : '');
 $due_date = (isset($_POST['due_date']) ?  $_POST['due_date'] : '');
 $detail = (isset($_POST['detail']) ?  $_POST['detail'] : '');
+
+$_record = GetTaskDetailOrg($task_id, $db);
+
+$mail_type = 0;
+
+if($_record[0]["status"] != $status)
+    $mail_type = 1;
+
+if($_record[0]["title"] != $title)
+    $mail_type = 2;
+
+if($_record[0]["priority"] != $priority)
+    $mail_type = 2;
+
+if($_record[0]["assignee"] != $assignee)
+    $mail_type = 2;
+
+if($_record[0]["collaborator"] != $collaborator)
+    $mail_type = 2;
+
+if($_record[0]["due_date"] != $due_date)
+    $mail_type = 2;
+
+if($_record[0]["due_time"] != $due_time)
+    $mail_type = 2;
+
+if($_record[0]["detail"] != $detail)
+    $mail_type = 2;
 
 try{
     $query = "update project_other_task_sl
@@ -89,6 +121,34 @@ try{
 
     if ($stmt->execute()) {
 
+        // send notify mail
+        if($mail_type == 1)
+            SendNotifyMail01($task_id, $_record[0]["status"], $username);
+
+        //if($mail_type == 2)
+        //    SendNotifyMail02($task_id, $_record[0]["status"]);
+
+        if($mail_type == 2)
+        {
+            $task_status = "";
+            switch ($status) {
+                case "0":
+                    $task_status = "Ongoing";
+                    break;
+                case "1":
+                    $task_status = "Pending";
+                    break;
+                case "2":
+                    $task_status = "Close";
+                    break;
+                case "-1":
+                    $task_status = "DEL";
+                    break;
+            }
+            $_record = GetTaskDetail($task_id, $db);
+            task_notify_admin_sl("edit", $task_status, $title, "", $_record[0]["create_id"], $assignee, $collaborator, $due_date . " " . $due_time, $detail, $task_id, $uid, 0);
+        }
+
         $returnArray = array('batch_id' => $task_id);
        
         $jsonEncodedReturnArray = json_encode($returnArray, JSON_PRETTY_PRINT);
@@ -106,3 +166,167 @@ catch (Exception $e)
     error_log($e->getMessage());
 }
 
+function SendNotifyMail01($last_id, $old_status_id, $username)
+{
+    $project_name = "";
+    $task_name = "";
+    $stages_status = "";
+    $create_id = "";
+
+    $assignee = "";
+    $collaborator = "";
+
+    $due_date = "";
+    $detail = "";
+
+    $stage_id = 0;
+
+    $_record = array();
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $_record = GetTaskDetail($last_id, $db);
+
+    $old_status = "";
+    switch ($old_status_id) {
+        case "0":
+            $old_status = "Ongoing";
+            break;
+        case "1":
+            $old_status = "Pending";
+            break;
+        case "2":
+            $old_status = "Close";
+            break;
+        case "-1":
+            $old_status = "DEL";
+            break;
+    }
+ 
+    $project_name = $_record[0]["project_name"];
+    $task_name = $_record[0]["task_name"];
+    $stages_status = $_record[0]["stages_status"];
+    $stages = $_record[0]["stage"];
+    $create_id = $_record[0]["create_id"];
+
+    $assignee = $_record[0]["assignee"];
+    $collaborator = $_record[0]["collaborator"];
+
+    $due_date = str_replace("-", "/", $_record[0]["due_date"]);
+    $detail = $_record[0]["detail"];
+
+    $stage_id = $_record[0]["stage_id"];
+    $task_status = $_record[0]["task_status"];
+
+    task_notify01_admin_sl($old_status, $task_status, $username, $task_name, $create_id, $assignee, $collaborator, $due_date, $detail, $last_id);
+
+}
+
+
+function SendNotifyMail02($last_id, $old_status_id)
+{
+    $project_name = "";
+    $task_name = "";
+    $stages_status = "";
+    $create_id = "";
+
+    $assignee = "";
+    $collaborator = "";
+
+    $due_date = "";
+    $detail = "";
+
+    $stage_id = 0;
+
+    $_record = array();
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $_record = GetTaskDetail($last_id, $db);
+
+    $old_status = "";
+    switch ($old_status_id) {
+        case "0":
+            $old_status = "Ongoing";
+            break;
+        case "1":
+            $old_status = "Pending";
+            break;
+        case "2":
+            $old_status = "Close";
+            break;
+        case "-1":
+            $old_status = "DEL";
+            break;
+    }
+ 
+    $project_name = $_record[0]["project_name"];
+    $task_name = $_record[0]["task_name"];
+    $stages_status = $_record[0]["stages_status"];
+    $stages = $_record[0]["stage"];
+    $create_id = $_record[0]["create_id"];
+
+    $assignee = $_record[0]["assignee"];
+    $collaborator = $_record[0]["collaborator"];
+
+    $due_date = str_replace("-", "/", $_record[0]["due_date"]);
+    $detail = $_record[0]["detail"];
+
+    $stage_id = $_record[0]["stage_id"];
+    $task_status = $_record[0]["task_status"];
+
+    task_notify02($old_status, $task_status, $project_name, $task_name, $stages, $stages_status, $create_id, $assignee, $collaborator, $due_date, $detail, $stage_id);
+
+}
+
+
+function GetTaskDetail($id, $db)
+{
+    $sql = "SELECT 0 stage_id, '' project_name, title task_name, 
+            '' `stages_status`, 
+            pt.create_id,
+            pt.assignee,
+            pt.collaborator,
+            due_date,
+            due_time,
+            '' stage,
+            (CASE pt.`status` WHEN '0' THEN 'Ongoing' WHEN '1' THEN 'Pending' WHEN '2' THEN 'Close' when '-1' then 'DEL' END ) as `task_status`, 
+            detail
+            FROM project_other_task_sl pt
+            WHERE pt.id = :id";
+
+    $merged_results = array();
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id',  $id);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $merged_results[] = $row;
+    }
+
+    return $merged_results;
+}
+
+
+function GetTaskDetailOrg($id, $db)
+{
+    $sql = "SELECT *
+            FROM project_other_task_sl pt
+
+            WHERE pt.id = :id";
+
+    $merged_results = array();
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id',  $id);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $merged_results[] = $row;
+    }
+
+    return $merged_results;
+}
