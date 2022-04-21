6d0bbb68fe4c4ed6dc7a7db8d2148411210b211d
dereckyin <dereckyin@users.noreply.github.com>
2022-04-21 16:41:17 +0800

    Create project_got_it.php

---
+++ api/project_got_it.php
@@ -0,0 +1,178 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$jwt = (isset($_COOKIE['jwt']) ?  $_COOKIE['jwt'] : null);
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+
+use \Firebase\JWT\JWT;
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+if (!isset($jwt)) {
+    http_response_code(401);
+
+    echo json_encode(array("message" => "Access denied."));
+    die();
+} else {
+    try {
+        // decode jwt
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+        $user_id = $decoded->data->id;
+        //if(!$decoded->data->is_admin)
+        //{
+        //  http_response_code(401);
+
+        //  echo json_encode(array("message" => "Access denied."));
+        //  die();
+        //}
+    }
+    // if decode fails, it means jwt is invalid
+    catch (Exception $e) {
+
+        http_response_code(401);
+
+        echo json_encode(array("message" => "Access denied."));
+        die();
+    }
+}
+
+header('Access-Control-Allow-Origin: *');
+
+include_once 'config/database.php';
+//include_once 'mail.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+switch ($method) {
+    
+    case 'POST':
+        // get database connection
+        $uid = $user_id;
+        $reply_id = (isset($_POST['reply_id']) ?  $_POST['reply_id'] : 0);
+        $message_id = (isset($_POST['message_id']) ?  $_POST['message_id'] : 0);
+        $kind = (isset($_POST['kind']) ?  $_POST['kind'] : '');
+    
+        $query = "INSERT INTO project_got_it
+        SET
+            `message_id` = :message_id,
+            `reply_id` = :reply_id,
+            `kind` = :kind,
+            `create_id` = :create_id,
+            `created_at` = now()";
+
+        // prepare the query
+        $stmt = $db->prepare($query);
+
+        // bind the values
+        $stmt->bindParam(':message_id', $message_id);
+        $stmt->bindParam(':reply_id', $reply_id);
+        $stmt->bindParam(':kind', $kind);
+        $stmt->bindParam(':create_id', $uid);
+
+        $last_id = 0;
+        // execute the query, also check if query was successful
+        try {
+            // execute the query, also check if query was successful
+            if ($stmt->execute()) {
+                $last_id = $db->lastInsertId();
+            } else {
+                $arr = $stmt->errorInfo();
+                error_log($arr[2]);
+            }
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+
+        // send notify mail
+        //SendNotifyMail($last_id, $uid);
+
+        $returnArray = array('batch_id' => $last_id);
+        $jsonEncodedReturnArray = json_encode($returnArray, JSON_PRETTY_PRINT);
+
+        echo $jsonEncodedReturnArray;
+
+        break;
+}
+
+
+
+function SendNotifyMail($last_id, $uid)
+{
+    $project_name = "";
+    $task_name = "";
+    $stages_status = "";
+    $create_id = "";
+
+    $assignee = "";
+    $collaborator = "";
+
+    $due_date = "";
+    $detail = "";
+
+    $stage_id = 0;
+
+    $_record = array();
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $_record = GetTaskDetail($last_id, $db);
+ 
+    $task_name = $_record[0]["task_name"];
+    $created_at = $_record[0]["created_at"];
+    $stages = "";
+    $create_id = $_record[0]["create_id"];
+
+    $assignee = $_record[0]["assignee"];
+    $collaborator = $_record[0]["collaborator"];
+
+    $stage_id = $_record[0]["id"];
+
+    $msg = $_record[0]["message"];
+
+    //$due_date = str_replace("-", "/", $_record[0]["due_date"]) . " " . $_record[0]["due_time"];
+    $detail = $_record[0]["detail"];
+
+    $username = $_record[0]["username"];
+    $_id = $_record[0]["_id"];
+
+    message_notify_dept("create", $project_name, $task_name, $stages, $create_id, $assignee, $collaborator, "", $detail, $stage_id, $msg, $username, $created_at, $_id, 'AD');
+
+}
+
+function GetTaskDetail($id, $db)
+{
+    $sql = "SELECT pt.id, title task_name, 
+            pt.create_id,
+            pt.assignee,
+            pt.collaborator,
+            due_date,
+            detail,
+            message,
+            u.username,
+            pmsg.created_at,
+            pmsg.create_id _id
+            FROM project_other_task_message_a pmsg
+            LEFT JOIN project_other_task_a pt ON pmsg.task_id = pt.id
+            LEFT JOIN user u ON u.id = pmsg.create_id
+            WHERE pmsg.id = :id";
+
+    $merged_results = array();
+
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':id',  $id);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $merged_results[] = $row;
+    }
+
+    return $merged_results;
+}
