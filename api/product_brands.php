7dc73ea84f8380c3b2c17de7c1c4e81e54a87dcd
dereckyin <dereckyin@users.noreply.github.com>
2022-05-24 11:13:20 +0800

    Update product_brands.php

--- api/product_brands.php
+++ api/product_brands.php
@@ -57,7 +57,7 @@ else
           case 'GET':
             $merged_results = array();
 
-            $sql = "SELECT distinct brand COLLATE UTF8MB4_BIN brand FROM product_category p  WHERE  p.STATUS <> -1 and p.brand <> '' order by p.brand";
+            $sql = "SELECT distinct brand COLLATE UTF8MB4_BIN brand FROM product_category p  WHERE  p.STATUS <> -1 and p.brand <> '' order by brand";
 
             $merged_results = array();
 
