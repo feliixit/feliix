a390169106413fb1068837e50a6be2567c21de08
dereckyin <dereckyin@users.noreply.github.com>
2022-09-22 09:37:19 +0800

    tag

--- order_taiwan_p1.php
+++ order_taiwan_p1.php
@@ -1775,6 +1775,7 @@
                                 <option value="LINEAR LIGHT">LINEAR LIGHT</option>
                                 <option value="PANEL LIGHT">PANEL LIGHT</option>
                                 <option value="PROJECTION LIGHT">PROJECTION LIGHT</option>
+                                <option value="TRACK BAR">TRACK BAR</option>
                                 <option value="TRACK LIGHT">TRACK LIGHT</option>
                                 <option value="TROFFER LIGHT">TROFFER LIGHT</option>
                                 <option value="LINEAR LIGHT">LINEAR LIGHT</option>
