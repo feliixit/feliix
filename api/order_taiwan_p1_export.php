57866476453dd569c4707d6b115e6a9cc49513ac
dereckyin <dereckyin@users.noreply.github.com>
2022-10-25 16:11:44 +0800

    Update order_taiwan_p1_export.php

--- api/order_taiwan_p1_export.php
+++ api/order_taiwan_p1_export.php
@@ -478,12 +478,12 @@ if($jwt){
             {
                 if($row['photo1'] != '')
                 {
-                    grab_image(str_replace(' ', '%20', $row['photo1']), $conf::$upload_path . str_replace(' ', '%20', $row['photo1']));
+                    grab_image($row['photo1'], $conf::$upload_path . $row['photo1']);
 
                     $objDrawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
                     $objDrawing->setName('photo1');
                     $objDrawing->setDescription('photo1');
-                    $objDrawing->setPath($conf::$upload_path  . str_replace(' ', '%20', $row['photo1']));
+                    $objDrawing->setPath($conf::$upload_path  . urlencode($row['photo1']));
                     $objDrawing->setCoordinates('B' . $i);
                     $objDrawing->setWidthAndHeight(100, 100);
                     $objDrawing->setResizeProportional(true);
@@ -913,8 +913,10 @@ function grab_image($image_url,$image_file){
     $object->downloadToFile($image_file);
     */
 
+    $image_file = urlencode($image_file);
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://storage.googleapis.com/feliiximg/' . $image_url);
+    curl_setopt($ch, CURLOPT_URL, 'https://storage.googleapis.com/feliiximg/' . urlencode($image_url));
     //Create a new file where you want to save
     $fp = fopen($image_file, 'w');
     curl_setopt($ch, CURLOPT_FILE, $fp);
