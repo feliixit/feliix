9eff09ae13fcbb161e7e79434684181f3d3a4070
dereckyin <dereckyin@users.noreply.github.com>
2022-11-23 13:54:46 +0800

    Update mail.php

--- api/mail.php
+++ api/mail.php
@@ -9178,14 +9178,14 @@ function GetAccessNotifiers($field, $order_type){
 
 function SetupMail($mail, $conf)
 {
-    $mail->SMTPDebug  = 0;
-    $mail->SMTPAuth   = true;
-    $mail->SMTPSecure = "ssl";
-    $mail->Port       = 465;
-    $mail->SMTPKeepAlive = true;
-    $mail->Host       = $conf::$mail_host;
-    $mail->Username   = $conf::$mail_username;
-    $mail->Password   = $conf::$mail_password;
+    // $mail->SMTPDebug  = 0;
+    // $mail->SMTPAuth   = true;
+    // $mail->SMTPSecure = "ssl";
+    // $mail->Port       = 465;
+    // $mail->SMTPKeepAlive = true;
+    // $mail->Host       = $conf::$mail_host;
+    // $mail->Username   = $conf::$mail_username;
+    // $mail->Password   = $conf::$mail_password;
 
     // $mail->SMTPDebug  = 0;
     // $mail->SMTPAuth   = true;
@@ -9196,14 +9196,14 @@ function SetupMail($mail, $conf)
     // $mail->Username   = 'sylvia99@ethereal.email';
     // $mail->Password   = '3YEXARcxXFhjT2k5RQ';
 
-    // $mail->SMTPDebug  = 0;
-    // $mail->SMTPAuth   = true;
-    // $mail->SMTPSecure = "tls";
-    // $mail->Port       = 587;
-    // $mail->SMTPKeepAlive = true;
-    // $mail->Host       = 'smtp.ethereal.email';
-    // $mail->Username   = 'shirley32@ethereal.email';
-    // $mail->Password   = '7uAyEy66YcXzpJZGZJ';
+    $mail->SMTPDebug  = 0;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+    $mail->SMTPKeepAlive = true;
+    $mail->Host       = 'smtp.ethereal.email';
+    $mail->Username   = 'ian58@ethereal.email';
+    $mail->Password   = 'enN1bKmRfPx77v5TMR';
 
      return $mail;
 
