afbca8f118b3dff7564fde9858646665efc3e40c
dereckyin <dereckyin@yahoo.com.tw>
2023-02-13 08:50:21 +0800

    Update knowledge_display.php

--- knowledge_display.php
+++ knowledge_display.php
@@ -505,8 +505,8 @@ catch (Exception $e){
                 </li>
 
                 <li>
-                    <a href="rec.link" target="_blank" v-if="rec.type == 'link' || rec.type == 'video'">{{rec.title}}</a>
-                    <a href="rec.attach" target="_blank" v-if="rec.type == 'file' ">{{rec.title}}</a>
+                    <a :href="rec.link" target="_blank" v-if="rec.type == 'link' || rec.type == 'video'">{{rec.title}}</a>
+                    <a :href="rec.attach" target="_blank" v-if="rec.type == 'file' ">{{rec.title}}</a>
                 </li>
 
                 <li>
