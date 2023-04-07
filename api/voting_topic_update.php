d706f78d83c02fa861cfeffd7be7f0bc72474e1c
dereckyin <dereckyin@yahoo.com.tw>
2023-04-07 11:52:16 +0800

    dd

--- api/voting_topic_update.php
+++ api/voting_topic_update.php
@@ -9,7 +9,7 @@ include_once 'libs/php-jwt-master/src/ExpiredException.php';
 include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-master/src/JWT.php';
 require_once '../vendor/autoload.php';
-
+include_once 'mail.php';
 
 use \Firebase\JWT\JWT;
 use Google\Cloud\Storage\StorageClient;
@@ -345,6 +345,8 @@ if (!isset($jwt)) {
                 }
                 
                 $db->commit();
+
+                send_mail($id, $db);
                 
                 http_response_code(200);
                 echo json_encode(array("message" => "Success at " . date("Y-m-d") . " " . date("h:i:sa")));
@@ -506,3 +508,108 @@ if (!isset($jwt)) {
                 }
             }
             
+            
+function send_mail($_id, $db)
+{
+    // get today by "Y-m-d"
+    $today = date("Y-m-d");
+
+    $sql = "select pm.id,
+                pm.topic, 
+                pm.access, 
+                pm.start_date, 
+                pm.end_date, 
+                pm.rule, 
+                pm.`status`,
+                pm.create_id,
+                c_user.username AS created_by, 
+                DATE_FORMAT(pm.created_at, '%Y-%m-%d %H:%i:%s') created_at 
+            from voting_template pm 
+            LEFT JOIN user c_user ON pm.create_id = c_user.id and pm.status <> -1 and pm.id = " . $_id;
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $username = $row['created_by'];
+        $created_at = $row['created_at'];
+        $topic = $row['topic'];
+        $review_start_date = $row['start_date'];
+        $review_end_date = $row['end_date'];
+        $od_id = $row['id'];
+        $cc_id = $row['create_id'];
+        $rule = $row['rule'];
+
+        // if today is review_start_date, send email
+        if ($today == $review_start_date)
+        {
+            // json to array
+            $access = json_decode($row['access'], true);
+            // convert to string
+            $access_str = implode(",", $access);
+            $receiver = knowledge_access_get($access_str, $db);
+
+            batch_voting_system_notify_mail($receiver, $cc_id, $topic, $username, $created_at, $review_start_date, $review_end_date,  GetRuleText($rule), $od_id, "start");
+        }
+
+        // if today is review_start_date, send email
+        if ($today == $review_end_date)
+        {
+            // json to array
+            $access = json_decode($row['access'], true);
+            // convert to string
+            $access_str = implode(",", $access);
+            $receiver = knowledge_access_get($access_str, $db);
+
+            batch_voting_system_notify_mail($receiver, "",  $topic, $username, $created_at, $review_start_date, $review_end_date,  GetRuleText($rule), $od_id, "end");
+            batch_voting_system_notify_mail($cc_id, "",  $topic, $username, $created_at, $review_start_date, $review_end_date,  GetRuleText($rule), $od_id, "mgt");
+        }
+    }
+}
+
+
+function knowledge_access_get($access, $db)
+{
+    $users = array();
+
+    $query = "select `user`.id,
+                username , email, department from `user` 
+            left join `user_department` on `user`.apartment_id = `user_department`.id
+            where `user`.status = 1";
+
+    $username = "";
+    $email = "";
+    $department = "";
+
+    $access_up = strtoupper($access); 
+
+    $stmt_cnt = $db->prepare( $query );
+    $stmt_cnt->execute();
+    while($row = $stmt_cnt->fetch(PDO::FETCH_ASSOC)) {
+        $uid = $row['id'];
+        $username = $row['username'];
+        $email = $row['email'];
+        $department = $row['department'];
+
+        // if username or department part of access then add to uses
+        if(strpos($access_up, strtoupper($username)) !== false || strpos($access_up, strtoupper($department)) !== false || strpos($access_up, "ALL") !== false)
+        {
+            $users[] = $uid;
+        }
+    }
+
+    return implode(",", $users);
+}
+
+function GetRuleText($rule)
+{
+    $rule_text = "";
+
+    if($rule == "1")
+        $rule_text = "one person - one vote";
+    else if($rule == "2")
+        $rule_text = "one person - two votes";
+    else if($rule == "3")
+        $rule_text = "one person - three votes";
+
+    return $rule_text;
+}
