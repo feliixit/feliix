b70f525051b30486c92c5e9a823eecefac026b81
dereckyin <dereckyin@yahoo.com.tw>
2023-08-08 10:22:02 +0800

    ;;

--- quotation.php
+++ quotation.php
@@ -2185,7 +2185,7 @@ header( 'location:index' );
             }
 
             .mainContent {
-                padding: 0 !important;;
+                padding: 0 !important;
                 background-color: #FFF !important;
             }
 
