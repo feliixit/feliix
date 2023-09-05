27255f6b624e04240a2257b16409fe2f5e9aaa9d
dereckyin <dereckyin@yahoo.com.tw>
2023-09-05 09:52:49 +0800

    Update quotation_mgt.php

--- api/quotation_mgt.php
+++ api/quotation_mgt.php
@@ -267,8 +267,8 @@ while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     // $post = GetRecentPost($row['id'], $db);
 
     $sorted_result[] = array(
-        "updated_at" => $row['updated_at'],
-        "username" => $row['username'],
+        "updated_at" => $row['created_at'],
+        "username" => $row['created_by'],
     
     );
     $post = $sorted_result;
