d17c24403ac54c7641ebf73f31098753eb943601
dereckyin <dereckyin>
2023-10-26 13:42:44 +0800

    $user_id == 154

--- admin/include/header.php
+++ admin/include/header.php
@@ -120,7 +120,7 @@ try {
         }
 
         $access3 = false;
-        if($user_id == 1 || $user_id == 4 || $user_id == 6 || $user_id == 2 || $user_id == 41 || $user_id == 3 || $user_id == 9 || $user_id == 87 || $user_id == 99 || $user_id == 139 || $user_id == 143 || $user_id == 146)
+        if($user_id == 1 || $user_id == 4 || $user_id == 6 || $user_id == 2 || $user_id == 41 || $user_id == 3 || $user_id == 9 || $user_id == 87 || $user_id == 99 || $user_id == 139 || $user_id == 143 || $user_id == 146 || $user_id == 154)
             $access3 = true;
 
         // 5. 針對 Reporting Section的內容，只有 Kristel Tan 和Thalassa Wren Benzon 和 Dennis Lin有權限可以進入和看到
